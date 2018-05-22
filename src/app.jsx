d2be7ea96d8eb75624c2f4048eff72208d3a1eb4
arthur <arthurwanggang@outlook.com>
2018-05-22 17:13:31 +0800

    配置文件

--- src/app.jsx
+++ src/app.jsx
@@ -2,13 +2,44 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import './index.scss';
-import 'font-awesome/css/font-awesome.min.css'
+
+
+//1：子组件想父组件传递数据
+
+class Father extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      color: '#aaa',
+      fontSzie: '30px'
+    }
+  }
+  render(){
+    return(
+      <div>Father</div>
+    )
+  }
+}
+
+
+class Child extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      color: 'red',
+      fontSzie: '50px'
+    }
+  }
+  render(){
+    return(
+      <div></div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <div> 
-      <i className="fa fa-address-book"></i>
-      <h1>Hello, world!</h1>,
+  <div>
+     <Father />
   </div>,
-  document.getElementById('app')
-);  
+  document.getElementById("app")
+)
